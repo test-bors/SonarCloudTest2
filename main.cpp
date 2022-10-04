044f4c8927345289b243bb8a409c1e77e069e5a2
Ruben Hinz <98730601+RuHiCo@users.noreply.github.com>
2022-10-04 13:32:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,4 +4,9 @@ int main(int argc, char *argv[]){
 	for(int i=0;i<3;i++){
 		std::cout<<"Hallo SonarCloud"<<std::endl;
 	}
+	if(1==1){
+		std::cout<<"Hallo Test"<<std::endl;
+	}else{
+		std::cout<<"Hallo kein Test"<<std::endl;
+	}
 }
