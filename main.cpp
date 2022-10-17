5015b38d616fccd242d60989cb440e397d84d11a
uie60741 <ruben.hinz@continental.com>
2022-10-17 14:39:52 +0200

    add exclusions, change main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@ int main(int argc, char *argv[]){
 	for(int i=0;i<3;i++){
 		std::cout<<"Hallo SonarCloud"<<std::endl;
 	}
-	if(1==1){
+	
+	for(int i=0;i<3;i++){
 		std::cout<<"Hallo Test"<<std::endl;
-	}else{
+	}
+	std::cin<<int x;
+	if(x<3){
 		std::cout<<"Hallo kein Test"<<std::endl;
 	}
 }
