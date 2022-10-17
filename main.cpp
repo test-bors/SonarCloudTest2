119206d6f314e2b7c1365c53b83f05829ef3b394
uie60741 <ruben.hinz@continental.com>
2022-10-17 14:43:51 +0200

    change main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[]){
 	for(int i=0;i<3;i++){
 		std::cout<<"Hallo Test"<<std::endl;
 	}
-	std::cin<<int x;
+	int x=0;
+	std::cin>>x;
 	if(x<3){
 		std::cout<<"Hallo kein Test"<<std::endl;
 	}
