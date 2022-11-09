ef093de2724cc58309ec134888de28749f2d96c5
uie60741 <ruben.hinz@continental.com>
2022-11-09 10:25:18 +0100

    add multiplication

--- main.cpp
+++ main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, char *argv[]){
-	for(int i=0;i<3;i++){
+	for(int i=0;i<2;i++){
 		std::cout<<"Hallo SonarCloud"<<std::endl;
 	}
 
-	for(int i=0;i<3;i++){
-		std::cout<<"Hallo Test"<<std::endl;
+	for(int i=0;i<5;i++){
+		std::cout<<"Hallo Sonar"<<std::endl;
 	}
+
 	int x=0;
 	std::cin>>x;
 	if(x<3){
-		std::cout<<"Hallo kein Test"<<std::endl;
+		std::cout<<"Hallo kein Test"<<x<<std::endl;
 	}
-	else if(x>1){
-		std::cout<<"Hallo kein Test"<<std::endl;
+	else if(x>5){
+		std::cout<<"Hallo ein Test"<<x<<std::endl;
 	}
-	else if(x<1){
-
+	int faktor1=1;
+	int faktor2=1;
+	std::cout<<"Wähle den ersten Faktor: ";
+	std::cin>>faktor1;
+	std::cout<<"Wähle den zweiten Faktor: ";
+	std::cin>>faktor2;
+	int sum=0;
+	while(faktor1>0){
+		if((faktor1%2)!=0){
+			sum+=faktor2;
+			std::cout<<std::setw(4)<<std::left<<faktor1<<std::right<<std::setw(6)<<faktor2<<std::endl;
+		}else{
+			std::cout<<std::setw(6)<<std::left<<faktor1<<std::right<<"-"<<std::setw(7)<<std::setfill('-')<<faktor2<<"-"<<std::setfill(' ')<<std::endl;
+		}
+		faktor1/=2;
+		faktor2*=2;
 	}
-	else{
-		std::cout<<"Hallo ein Test"<<std::endl;
-	}
+	std::cout<<"---------------"<<std::endl<<std::setw(10)<<sum<<std::endl;
 }
